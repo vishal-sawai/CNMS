1d1d54262a4250c92f13929f4ece0935fd6c2993
vishal sawai <vishalsawai1725@gmail.com>
2023-02-03 20:33:02 +0530

    Add login page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 // navbar button slide up and down
 const menu = document.getElementById('mobile-menu');
 const toggle = () => menu.classList.toggle("hidden");
+
+// Student login and teacher login
+function StudentLogin() {
+    document.getElementById("StudentForm").style.display = "block";
+    document.getElementById("TeacherForm").style.display = "none";
+    document.getElementById("StudentBtn").style.backgroundColor = "darkred";
+    document.getElementById("TeacherBtn").style.backgroundColor = "#2C3E50";
+}
+function TeacherLogin() {
+    document.getElementById("StudentForm").style.display = "none";
+    document.getElementById("TeacherForm").style.display = "block";
+    document.getElementById("TeacherBtn").style.backgroundColor = "darkred";
+    document.getElementById("StudentBtn").style.backgroundColor = "#2C3E50";
+}
