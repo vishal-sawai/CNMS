f84aa5d2e8cecbb8d403a6c70edd0089c0800106
vishal sawai <vishalsawai1725@gmail.com>
2023-02-11 00:03:46 +0530

    add new

--- js/script.js
+++ js/script.js
@@ -25,3 +25,29 @@ const navprofile = () => navprofilemenu.classList.toggle("hidden");
 function formdata(val1) {
     document.getElementById("coursevalue").value = val1;
 }
+
+
+// Admin dashboard page
+// <!-- section hide and show code -->
+var divs = ["Section1", "Section2", "Section3"];
+var visibleId = null;
+
+function show(id) {
+    if (visibleId !== id) {
+        visibleId = id;
+    }
+    hide();
+}
+
+function hide() {
+    var div, i, id;
+    for (i = 0; i < divs.length; i++) {
+        id = divs[i];
+        div = document.getElementById(id);
+        if (visibleId === id) {
+            div.style.display = "block";
+        } else {
+            div.style.display = "none";
+        }
+    }
+}
